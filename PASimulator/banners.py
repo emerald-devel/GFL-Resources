1182243e4f7e45d436bfb5ee99b812bfe2f09762
Assassin Eclipse <beepbeep11@live.com>
2021-07-05 23:49:23 +0800

    Temporary placeholders for all future banners

--- PASimulator/banners.py
+++ PASimulator/banners.py
@@ -24,3 +24,75 @@ def get_banner(name, prio):
                 {'name': 'Striker', 'rarity': 1, 'prio': prio[8], 'count': 10},
                 {'name': 'Scout', 'rarity': 1, 'prio': prio[9], 'count': 10},
                 {'name': 'Prowler', 'rarity': 1, 'prio': prio[10], 'count': 11}]
+
+    if name == 'Hunter':
+        return [{'name': 'Hunter', 'rarity': 3, 'prio': prio[0], 'count': 1},
+                {'name': 'Manticore', 'rarity': 2, 'prio': prio[1], 'count': 5},
+                {'name': 'Other 2*', 'rarity': 2, 'prio': prio[2], 'count': 23},
+                {'name': '1*', 'rarity': 1, 'prio': prio[3], 'count': 71}]
+
+    if name == 'Intruder':
+        return [{'name': 'Intruder', 'rarity': 3, 'prio': prio[0], 'count': 1},
+                {'name': 'Manticore', 'rarity': 2, 'prio': prio[1], 'count': 5},
+                {'name': 'Other 2*', 'rarity': 2, 'prio': prio[2], 'count': 23},
+                {'name': '1*', 'rarity': 1, 'prio': prio[3], 'count': 71}]
+
+    if name == 'Destroyer':
+        return [{'name': 'Destroyer', 'rarity': 3, 'prio': prio[0], 'count': 1},
+                {'name': 'Manticore', 'rarity': 2, 'prio': prio[1], 'count': 4},
+                {'name': 'Other 2*', 'rarity': 2, 'prio': prio[2], 'count': 24},
+                {'name': '1*', 'rarity': 1, 'prio': prio[3], 'count': 71}]
+
+    if name == 'Architect':
+        return [{'name': 'Architect', 'rarity': 3, 'prio': prio[0], 'count': 1},
+                {'name': 'Manticore', 'rarity': 2, 'prio': prio[1], 'count': 4},
+                {'name': 'Other 2*', 'rarity': 2, 'prio': prio[2], 'count': 24},
+                {'name': '1*', 'rarity': 1, 'prio': prio[3], 'count': 71}]
+
+    if name == 'Ouroboros':
+        return [{'name': 'Ouroboros', 'rarity': 3, 'prio': prio[0], 'count': 1},
+                {'name': 'Manticore', 'rarity': 2, 'prio': prio[1], 'count': 3},
+                {'name': 'Other 2*', 'rarity': 2, 'prio': prio[2], 'count': 25},
+                {'name': '1*', 'rarity': 1, 'prio': prio[3], 'count': 71}]
+
+    if name == 'Alchemist':
+        return [{'name': 'Alchemist', 'rarity': 3, 'prio': prio[0], 'count': 1},
+                {'name': 'Manticore', 'rarity': 2, 'prio': prio[1], 'count': 3},
+                {'name': 'Other 2*', 'rarity': 2, 'prio': prio[2], 'count': 25},
+                {'name': '1*', 'rarity': 1, 'prio': prio[3], 'count': 71}]
+
+    if name == 'Dreamer':
+        return [{'name': 'Dreamer', 'rarity': 3, 'prio': prio[0], 'count': 1},
+                {'name': 'Manticore', 'rarity': 2, 'prio': prio[1], 'count': 2},
+                {'name': 'Other 2*', 'rarity': 2, 'prio': prio[2], 'count': 26},
+                {'name': '1*', 'rarity': 1, 'prio': prio[3], 'count': 71}]
+
+    if name == 'Gager':
+        return [{'name': 'Gager', 'rarity': 3, 'prio': prio[0], 'count': 1},
+                {'name': 'Manticore', 'rarity': 2, 'prio': prio[1], 'count': 2},
+                {'name': 'Other 2*', 'rarity': 2, 'prio': prio[2], 'count': 26},
+                {'name': '1*', 'rarity': 1, 'prio': prio[3], 'count': 71}]
+
+    if name == 'Judge':
+        return [{'name': 'Judge', 'rarity': 3, 'prio': prio[0], 'count': 1},
+                {'name': 'Manticore', 'rarity': 2, 'prio': prio[1], 'count': 2},
+                {'name': 'Other 2*', 'rarity': 2, 'prio': prio[2], 'count': 26},
+                {'name': '1*', 'rarity': 1, 'prio': prio[3], 'count': 71}]
+
+    if name == 'Agent':
+        return [{'name': 'Agent', 'rarity': 3, 'prio': prio[0], 'count': 1},
+                {'name': 'Manticore', 'rarity': 2, 'prio': prio[1], 'count': 2},
+                {'name': 'Other 2*', 'rarity': 2, 'prio': prio[2], 'count': 26},
+                {'name': '1*', 'rarity': 1, 'prio': prio[3], 'count': 71}]
+
+    if name == 'Adeline':
+        return [{'name': 'Adeline', 'rarity': 3, 'prio': prio[0], 'count': 1},
+                {'name': 'Manticore', 'rarity': 2, 'prio': prio[1], 'count': 2},
+                {'name': 'Other 2*', 'rarity': 2, 'prio': prio[2], 'count': 26},
+                {'name': '1*', 'rarity': 1, 'prio': prio[3], 'count': 71}]
+
+    if name == 'Alina':
+        return [{'name': 'Alina', 'rarity': 3, 'prio': prio[0], 'count': 1},
+                {'name': 'Manticore', 'rarity': 2, 'prio': prio[1], 'count': 2},
+                {'name': 'Other 2*', 'rarity': 2, 'prio': prio[2], 'count': 26},
+                {'name': '1*', 'rarity': 1, 'prio': prio[3], 'count': 71}]
